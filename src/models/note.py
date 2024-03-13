92547437a22536ae5e6c89c4fb9a8e1d859de12e
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2024-03-13 19:03:44 +0200

    add new methods to Note

--- src/models/note.py
+++ src/models/note.py
@@ -3,6 +3,8 @@ from src.models.title import Title
 from src.models.content import Content
 from src.errors.errors import ValidationError
 from src.errors.error_messages import invalid_note_id_format_error_message
+from src.errors.error_messages import tag_already_exists_error_message_template
+from src.errors.error_messages import tag_doenst_exist_error_message_template
 
 
 class Note:
@@ -20,6 +22,28 @@ class Note:
                 and self.tags == other.tags
             )
         return False
+
+    def change_title(self, title: str) -> None:
+        self.title = Title(title.strip())
+
+    def change_content(self, content: str) -> None:
+        self.content = Content(content.strip())
+
+    def add_tag(self, tag: str) -> None:
+        new_tag = Tag(tag.strip())
+        if new_tag in self.tags:
+            raise ValidationError(
+                tag_already_exists_error_message_template.format(tag=new_tag.value, id=self.id))
+        self.tags.append(new_tag)
+
+    def change_tag(self, old_tag: str, new_tag: str) -> None:
+        existing_tag = Tag(old_tag.strip())
+        if existing_tag not in self.tags:
+            raise ValidationError(
+                tag_doenst_exist_error_message_template.format(tag=existing_tag.value, id=self.id))
+        for index, t in enumerate(self.tags):
+            if existing_tag == t:
+                self.tags[index] = Tag(new_tag.strip())
 
     def __hash__(self):
         return hash(self.title, self.content, tuple(self.tags))
