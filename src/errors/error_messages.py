5fbde65a59aac4ed036822ad179f208e07870360
KatanovaLesya <katanovalesya@gmail.com>
2024-03-13 23:26:06 +0200

    six

--- src/errors/error_messages.py
+++ src/errors/error_messages.py
@@ -70,3 +70,19 @@ show_all_birthdays_error_messages = {
 parse_input_error_messages = {
     'FormatError': generic_invalid_command_format_message,
 }
+
+change_title_error_messages = {
+    'KeyError': 'Note with provided ID does not exist'
+}
+
+change_content_error_messages = {
+    'KeyError': 'Note with provided ID does not exist'
+}
+
+add_tag_error_messages = {
+    'KeyError': 'Note with provided ID does not exist'
+}
+
+change_tag_error_messages = {
+    'KeyError': 'Note with provided ID does not exist'
+}
