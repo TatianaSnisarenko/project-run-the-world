54affe9d2fa9e25c9b1135343869d7b797d56cf2
daria_hk <honcharuk.daria95@gmail.com>
2024-03-16 15:24:16 +0100

    user_birthdays.py functions description

--- assistant/src/user_birthdays.py
+++ assistant/src/user_birthdays.py
@@ -4,12 +4,31 @@ from calendar import isleap
 
 
 def get_birthdays_per_week(users, per_days):
+    """Returns a tuple containing upcoming birthdays of users within a specified number of days.
+
+    Args:
+        users (list): A list of users for whom to retrieve birthdays.
+        per_days (int): The number of days within which to search for upcoming birthdays.
+
+    Returns:
+        tuple: A tuple containing upcoming birthdays of users within the specified number of days.
+    """
     if not users:
         return tuple()
     return get_birthdays_per_week_from_date(users, datetime.today(), per_days)
 
 
 def get_birthdays_per_week_from_date(users, from_date, per_days):
+    """Returns upcoming birthdays of users within a specified number of days from a given date.
+
+    Args:
+        users (list): A list of dictionaries containing user data with 'name' and 'birthday' keys.
+        from_date (datetime.date): The reference date from which to calculate upcoming birthdays.
+        per_days (int): The number of days within which to search for upcoming birthdays.
+
+    Returns:
+        list: A list of dictionaries containing 'Birthday' and 'Name' keys, representing upcoming birthdays within the specified number of days from the reference date.
+    """
     date = from_date.date()
 
     result = defaultdict(list)
@@ -45,11 +64,28 @@ def get_birthdays_per_week_from_date(users, from_date, per_days):
 
 
 def show_birthdays_per_week_from_date(users_birthdays):
+    """Prints the upcoming birthdays of users within a week from a given date.
+
+    Args:
+        users_birthdays (list): A list of dictionaries containing 'Birthday' and 'Name' keys, representing upcoming birthdays of users within a week from a specified date.
+    """
     for value in users_birthdays:
         print(value)
 
 
 def get_date(year, month, day):
+    """Returns a date object for the given year, month, and day.
+
+    If the given year is not a leap year and the month is February with a day of 29, the day is adjusted to 1st of March.
+
+    Args:
+        year (int): The year.
+        month (int): The month (1-12).
+        day (int): The day of the month.
+
+    Returns:
+        datetime.date: A date object representing the given date.
+    """
     if not isleap(year) and month == 2 and day == 29:
         month = 3
         day = 1
